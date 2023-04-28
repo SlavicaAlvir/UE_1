0c8b55c94eed22c2a8ca5e9f511d84b4e4ef2a21
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-04-28 14:24:48 +0200

    added comment

--- age_calculator.py
+++ age_calculator.py
@@ -3,7 +3,7 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+# Test
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     text = "Hello my name is {name} an I am {age} years old"
@@ -20,3 +20,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
