de6371f82ebd44418363d18242c962f3fd37cece
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-04-28 14:22:03 +0200

    Übung finished

---
+++ age_calculator.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    text = "Hello my name is {name} an I am {age} years old"
+
+    print(text.format(name="Slavica", age=27))
+
+    name2 = input("What is your name?")
+
+    print(name2 + " nice to meet you")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
